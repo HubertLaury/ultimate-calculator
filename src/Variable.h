9d616cce850e6b112bae03dec7e49e99a6d21112
HubertLaury <hubert.laury@hotmail.fr>
2021-05-04 16:14:08 +0200

    Create Variable.h

---
+++ src/Variable.h
@@ -0,0 +1,14 @@
+#ifndef VARIABLE_H
+#define VARIABLE_H
+
+#include "Constante.h"
+
+class Variable : public Constante
+{
+public:
+//l'appel a toutes les fonctions
+
+private:
+std::string _nom;
+};
+#endif // VARIABLE_H
